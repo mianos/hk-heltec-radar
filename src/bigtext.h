3f880e860b7d5323307020614112bffb3caf97c2
Rob Fowler <rfo@mianos.com>
2023-08-20 17:06:13 +1000

    Class for distance

---
+++ src/bigtext.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <Fonts/FreeSansBold24pt7b.h>
+
+class BigText {
+  uint16_t num_height;
+  uint16_t max_width = 0;
+
+  Adafruit_SSD1306& display;
+
+  void calculateHeight() {
+    display.setFont(&FreeSansBold24pt7b);
+    
+    int16_t x1, y1; // Position of upper-left corner of the bounding box
+    uint16_t w, h; // Width and height of bounding box
+
+    // Use a test string with digits and a decimal point
+    String testStr = "0.00";
+
+    // Calculate the bounds of the text
+    display.getTextBounds(testStr, 0, 0, &x1, &y1, &w, &h);
+
+    // Set the calculated height
+    num_height = h;
+  }
+
+public:
+  BigText(Adafruit_SSD1306& displayInstance) : display(displayInstance) {
+    calculateHeight();
+  }
+
+
+  void displayLargeDistance(float distance, int x = 0, int y = 0) {
+    // Convert distance to a String with N.NN format
+    String distanceStr = String(distance, 2);
+
+    // Set the font for the large text
+    display.setFont(&FreeSansBold24pt7b);
+
+    // Calculate the width of the current distance string
+    int16_t x1, y1; // Position of upper-left corner of the bounding box
+    uint16_t w, h; // Width and height of bounding box
+    display.getTextBounds(distanceStr, 0, 0, &x1, &y1, &w, &h);
+
+    // Clear the maximum width so far, starting at the specified x and y offset
+    // Extend the clearing area slightly to the right and bottom
+    display.fillRect(x, 8 + y, max_width + 1, num_height + 2, SSD1306_BLACK);
+
+    // Display the distance below the scroller, starting at the specified x and y offset
+    display.setCursor(x, 8 + y + num_height);
+    display.println(distanceStr);
+    display.display();
+
+    // Update the maximum width if the current width is greater
+    if (w > max_width) {
+      max_width = w;
+    }
+  }
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,67 +4,16 @@
 #include "lwifi.h"
 
 
-#include <Fonts/FreeSansBold24pt7b.h>
-
+#include "bigtext.h"
 #include "scroller.h"
-
 
 
 
 TwoWire twi = TwoWire(1); // create our own TwoWire instance
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &twi, OLED_RESET);
 
-
 ScrollingText scroller(display);
-
-static uint16_t num_height;
-
-void calculateHeight() {
-  display.setFont(&FreeSansBold24pt7b);
-  
-  int16_t x1, y1; // Position of upper-left corner of the bounding box
-  uint16_t w, h; // Width and height of bounding box
-
-  // Use a test string with digits and a decimal point
-  String testStr = "0.00";
-
-  // Calculate the bounds of the text
-  display.getTextBounds(testStr, 0, 0, &x1, &y1, &w, &h);
-
-  // Set the calculated height
-  num_height = h;
-}
-
-static int max_width = 0; // Keep track of the maximum width
-
-void displayLargeDistance(float distance, int x = 0, int y = 0) {
-  // Convert distance to a String with N.NN format
-  String distanceStr = String(distance, 2);
-
-  // Set the font for the large text
-  display.setFont(&FreeSansBold24pt7b);
-
-  // Calculate the width of the current distance string
-  int16_t x1, y1; // Position of upper-left corner of the bounding box
-  uint16_t w, h; // Width and height of bounding box
-  display.getTextBounds(distanceStr, 0, 0, &x1, &y1, &w, &h);
-
-  // Clear the maximum width so far, starting at the specified x and y offset
-  // Extend the clearing area slightly to the right and bottom
-  display.fillRect(x, 8 + y, max_width + 1, num_height + 2, SSD1306_BLACK);
-
-  // Display the distance below the scroller, starting at the specified x and y offset
-  display.setCursor(x, 8 + y + num_height);
-  display.println(distanceStr);
-  display.display();
-
-  // Update the maximum width if the current width is greater
-  if (w > max_width) {
-    max_width = w;
-  }
-}
-
-
+BigText bigText(display);
 
 float ff = 0.0;
 
@@ -88,7 +37,6 @@ void setup() {
     Serial.printf("SSD1306 allocation failed\n");
     for(;;) {}
 	}
-	calculateHeight();
 	display.clearDisplay();
   display.display();
 
@@ -103,6 +51,6 @@ void setup() {
 void loop() {
   scroller.scroll();
   float distance = getDistance(); // Replace with your method to get the distance
-  displayLargeDistance(distance, 10, 8);
+  bigText.displayLargeDistance(distance, 10, 8);
   delay(10);
 }
