0f85aeb45a744050aac5f80f2bf20485f3c4b73c
Rob Fowler <rfo@mianos.com>
2023-09-07 09:22:32 +1000

    Making event manager dynamic so I can set up a device with no display

--- src/main.cpp
+++ src/main.cpp
@@ -26,26 +26,32 @@ RadarMqtt mqtt{scroller};
 bool network_up{false};
 
 class LocalEP : public EventProc {
+  BigText *bigText;
+  PowerLine *powerLine;
+  RadarMqtt *mqtt;
 public:
+  LocalEP(BigText *bigText, PowerLine *powerLine, RadarMqtt *mqtt) : bigText(bigText), powerLine(powerLine), mqtt(mqtt) {
+  }
+
   virtual void Detected(String& type, float distanceValue, float strengthValue, bool entry) {
-    bigText.displayLargeDistance(distanceValue, 10, 8);
-    powerLine.show(strengthValue);
+    bigText->displayLargeDistance(distanceValue, 10, 8);
+    powerLine->show(strengthValue);
     if (network_up) {
       if (entry) {
-        mqtt.mqtt_update_presence(entry, false, distanceValue, strengthValue);
+        mqtt->mqtt_update_presence(entry, false, distanceValue, strengthValue);
       } else {
-        mqtt.mqtt_update_presence(entry, true, distanceValue, strengthValue);
+        mqtt->mqtt_update_presence(entry, true, distanceValue, strengthValue);
       }
     }
   }
   virtual void Cleared() {
-    bigText.displayLargeDistance(0.0, 10, 8);
-    powerLine.show(0);
+    bigText->displayLargeDistance(0.0, 10, 8);
+    powerLine->show(0);
     if (network_up) {
-      mqtt.mqtt_update_presence(false);
+      mqtt->mqtt_update_presence(false);
     }
   }
-} lep;
+};
 
 RadarSensor *radarSensor;
 
@@ -65,20 +71,22 @@ void setup() {
   // Example: You can put static content here that will remain on the display
   scroller.startScrolling();
 
+  auto *lep = new LocalEP{&bigText, &powerLine, &mqtt};
+
   if (!strcmp(radar_module, "ld2411")) {
-    radarSensor = new LD2411{&lep};
+    radarSensor = new LD2411{lep};
     Serial.printf("LD2411  radar module type '%s'\n", radar_module);
   } else if (!strcmp(radar_module, "ld1125")) {
-    radarSensor = new LD1125{&lep};
+    radarSensor = new LD1125{lep};
     Serial.printf("LD1125  radar module type '%s'\n", radar_module);
   } else if (!strcmp(radar_module, "ld2410")) {
-    radarSensor = new LD2410{&lep};
+    radarSensor = new LD2410{lep};
     Serial.printf("LD2410  radar module type '%s'\n", radar_module);
   } else {
     scroller.taf("Undefined radar module type '%s'\n", radar_module);
     Serial.printf("Undefined radar module type '%s' using LD1125\n", radar_module);
     // Using any one as not to have null calls.
-    radarSensor = new LD2411{&lep};
+    radarSensor = new LD2411{lep};
   }
 
   wifi_connect();
