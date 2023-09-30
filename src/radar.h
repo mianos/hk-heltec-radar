4ae9ee96fb4c38581b956d196fc79cd786203030
Rob Fowler <rfo@mianos.com>
2023-10-01 10:17:43 +1100

    Added filtering of the same value across readings

--- src/radar.h
+++ src/radar.h
@@ -2,76 +2,135 @@
 #include <vector>
 #include <memory>
 
-
 class EventProc {
 public:
-  virtual void Detected(String& type, float value, float power, bool entry, bool speed_type=false) = 0;
-  virtual void Cleared() = 0;
+    virtual void Detected(String& type, float value, float power, bool entry, bool speed_type=false) = 0;
+    virtual void Cleared() = 0;
 };
 
 struct Value {
-  float power;
-  virtual const char* etype() = 0;
-  virtual void print() {
-    Serial.printf("un-overridden '%s' dpower %g\n", etype(), power);
-  }
-  Value(float power=0.0) : power(power) {}
+    float power;
+
+    virtual const char* etype() const = 0;
+
+    virtual bool isEqual(const Value& other) const {
+        return this->power == other.power;
+    }
+
+    virtual Value* clone() const = 0;
+
+    virtual void print() {
+        Serial.printf("un-overridden '%s' dpower %g\n", etype(), power);
+    }
+
+    Value(float power=0.0) : power(power) {}
 };
 
 struct Movement : public Value {
-  float distance = 0.0;
-  const char* etype() override { return "mov"; }
-  Movement(float distance, float power=0.0) : Value(power), distance(distance) {}
-  virtual void print() {
-    Serial.printf("mov distance %2.1f power %2.1f\n", distance, power);
-  }
+    float distance = 0.0;
+
+    const char* etype() const override { return "mov"; }
+
+    bool isEqual(const Value& other) const override {
+        const Movement* otherMov = static_cast<const Movement*>(&other);
+        return this->distance == otherMov->distance && this->power == other.power;
+    }
+
+    Movement* clone() const override {
+        return new Movement(*this);
+    }
+
+    void print() override {
+        Serial.printf("mov distance %2.1f power %2.1f\n", distance, power);
+    }
+
+    Movement(float distance, float power=0.0) : Value(power), distance(distance) {}
 };
 
 struct Occupancy : public Value {
-  float distance = 0.0;
-  const char* etype() override { return "occ"; }
-  float value() {
-    return distance;
-  }
-  Occupancy(float distance, float power=0.0) : Value(power), distance(distance) {}
-  virtual void print() {
-    Serial.printf("occ distance %2.1f power %2.1f\n", distance, power);
-  }
-};
+    float distance = 0.0;
 
+    const char* etype() const override { return "occ"; }
+
+    bool isEqual(const Value& other) const override {
+        const Occupancy* otherOcc = static_cast<const Occupancy*>(&other);
+        return this->distance == otherOcc->distance && this->power == other.power;
+    }
+
+    Occupancy* clone() const override {
+        return new Occupancy(*this);
+    }
+
+    void print() override {
+        Serial.printf("occ distance %2.1f power %2.1f\n", distance, power);
+    }
+
+    Occupancy(float distance, float power=0.0) : Value(power), distance(distance) {}
+};
 
 struct Speed : public Value {
-  float speed = 0.0;
-  const char* etype() override { return "spd"; }
-  Speed(float speed) : speed(speed) {}
-  virtual void print() {
-    Serial.printf("spd %2.2f\n", speed);
-  }
-};
+    float speed = 0.0;
 
+    const char* etype() const override { return "spd"; }
+
+    bool isEqual(const Value& other) const override {
+        const Speed* otherSpd = static_cast<const Speed*>(&other);
+        return this->speed == otherSpd->speed;
+    }
+
+    Speed* clone() const override {
+        return new Speed(*this);
+    }
+
+    void print() override {
+        Serial.printf("spd %2.2f\n", speed);
+    }
+
+    Speed(float speed) : speed(speed) {}
+};
 
 struct Range : public Value {
-  float x = 0.0;
-  float y = 0.0;
-  float speed;
-  int reference;
-  const char* etype() override { return "rng"; }
-  virtual void print() {
-    Serial.printf("speed %1.2f x pos %1.2f Y pos %1.2f %2d\n", speed, x, y, reference);
-  }
-  Range(float x, float y, float speed, int reference=0) : x(x), y(y), speed(speed), reference(reference) {}
-};
+    float x = 0.0;
+    float y = 0.0;
+    float speed;
+    int reference;
 
+    const char* etype() const override { return "rng"; }
+
+    bool isEqual(const Value& other) const override {
+        const Range* otherRange = static_cast<const Range*>(&other);
+        return this->x == otherRange->x && this->y == otherRange->y &&
+               this->speed == otherRange->speed && this->reference == otherRange->reference &&
+               this->power == other.power;
+    }
+
+    Range* clone() const override {
+        return new Range(*this);
+    }
+
+    void print() override {
+        Serial.printf("speed %1.2f x pos %1.2f Y pos %1.2f %2d\n", speed, x, y, reference);
+    }
+
+    Range(float x, float y, float speed, int reference=0) : Value(0), x(x), y(y), speed(speed), reference(reference) {}
+};
 
 struct NoTarget : public Value {
-  const char* etype() override { return "no"; }
-  virtual void print() {
-    Serial.printf("no target\n");
-  }
+    const char* etype() const override { return "none"; }
+
+    NoTarget* clone() const override {
+        return new NoTarget();
+    }
+
+    void print() override {
+        Serial.printf("no target\n");
+    }
 };
+
 
 class RadarSensor {
   EventProc* ep;
+  std::vector<std::unique_ptr<Value>> lastValuesList;
 public:
   RadarSensor(EventProc* ep) : ep(ep) {}
 
@@ -100,13 +159,29 @@ public:
     Serial.println("Cleared");
   }
 
+
+  bool areValuesListsSame(const std::vector<std::unique_ptr<Value>>& list1,
+                          const std::vector<std::unique_ptr<Value>>& list2) {
+      if (list1.size() != list2.size()) return false;
+
+      for (size_t i = 0; i < list1.size(); i++) {
+          if (!list1[i]->isEqual(*list2[i])) return false;
+      }
+
+      return true;
+  }
+
   void process(float minPower = 0.0) {
     auto valuesList = get_decoded_radar_data();
+
+    if (areValuesListsSame(valuesList, lastValuesList)) {
+      return; // Exit early if the lists are the same
+    }
 
     bool noTargetFound = true;
     for (auto &v : valuesList) {
         //v->print();
-        if (v->etype() == "no") {
+        if (v->etype() == "none") {
             if (currentState == STATE_DETECTED || currentState == STATE_DETECTED_ONCE) {
                 Cleared();
                 currentState = STATE_CLEARED_ONCE;
@@ -150,8 +225,12 @@ public:
             currentState = STATE_NOT_DETECTED;
             break;
     }
-
+   lastValuesList.clear();
+    for (auto& v : valuesList) {
+        lastValuesList.push_back(std::unique_ptr<Value>(v->clone()));
+    }
   }
+
   //ep->Detected(eventType, eventValue, eventPower, true, speed_type);
   //  ep->Cleared();
 };
